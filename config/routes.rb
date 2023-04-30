d5f4326efc7c671b8c9721839524e26b7902854f
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-04-30 09:52:05 +0000

    [now]1-16

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ devise_for :users
 #get 'homes/top'
 #get 'about' => 'homes#about'
 
-resources :post_images, only: [:new, :create, :index, :show]
+resources :post_images, only: [:new, :create, :index, :show, :destroy]
+resources :users, only: [:show, :edit]
 
 get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
